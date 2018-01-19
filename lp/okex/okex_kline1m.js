4b2c21de51aabbfe6781d19e76f7d7c49a65540c
charlieholy <357483226@qq.com>
2018-01-19 18:00:45 +0800

    add db and try log4j add ping pong

--- lp/okex/okex_kline1m.js
+++ lp/okex/okex_kline1m.js
@@ -22,23 +22,44 @@ console.log("substr: " + substr);
 let WebSocket = require('ws');
 
 const socket = new WebSocket(url); //如果symbol = 'btccny'或者'ltccny' 请使用wss://api.huobi.com/ws
+var okCoinWebSocket = {}
+okCoinWebSocket["overtime"] = 8000;
+function checkConnect() {
+    socket.send("{'event':'ping'}");
+    if ((new Date().getTime() - okCoinWebSocket.lastHeartBeat) > okCoinWebSocket.overtime) {
+        onsole.log("socket 连接断开，正在尝试重新建立连接");
+        //testWebSocket();
+    }
+}
+
+
+
 
 
 socket.onopen = function (event) {
     console.log('WebSocket connect at time: ' + new Date());
-    socket.send("ping");
     socket.send(substr);
+    setInterval(checkConnect,5000);
 };
 
 socket.onmessage = function (event) {
     let raw_data = event.data;
     // console.log(raw_data);
     // sub [{}]
-    if(raw_data == "pong")
-
+    raw_data = "asd";
+    try {
+        var jdata = JSON.parse(raw_data);
+    }
+    catch (e)
+    {
+        console.log("e: " + e );
+        return;
+    }
+    if (jdata.event == 'pong') {
+        okCoinWebSocket.lastHeartBeat = new Date().getTime();
         return
-    var subdata = raw_data.substr(1,raw_data.length-2)
-    console.log(subdata);
+    }
+    console.log(raw_data);
 };
 
 socket.onclose = function(event) {
