7a8880237d5604a68b491a9bc0f174b201fc4c41
charlieholy <357483226@qq.com>
2018-01-26 09:30:10 +0800

    add ltc eth

---
+++ eth_kline1m.js
@@ -0,0 +1,4 @@
+require("./lp/huopro/huopro_eth_kline1m")
+require("./lp/okex/okex_eth_kline1m")
+require("./lp/binance/binance_eth_kline1m")
+require("./lp/bitfinex/bitfinex_eth_kline1m")
---
+++ ltc_kline1m.js
@@ -0,0 +1,4 @@
+require("./lp/huopro/huopro_ltc_kline1m")
+require("./lp/okex/okex_ltc_kline1m")
+require("./lp/binance/binance_ltc_kline1m")
+require("./lp/bitfinex/bitfinex_ltc_kline1m")
