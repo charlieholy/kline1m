76e32efaa1603b67acc81fd5cf09789f3d15acc5
charlieholy <357483226@qq.com>
2018-01-29 11:36:52 +0800

    add crashlog

--- log4js/Loger.js
+++ log4js/Loger.js
@@ -1,13 +1,30 @@
 var log4js = require('log4js');
 log4js.configure({
-    appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-    categories: { default: { appenders: ['cheese'], level: 'debug' } },
+    appenders: {
+        stdout: {//控制台输出
+            type: 'stdout'
+        },
+        cheese: { type: 'file', filename: 'cheese.log' },
+        crash: {//错误日志
+            type: 'dateFile',
+            filename: 'crash',
+            pattern: '-yyyy-MM-dd-hh-mm-ss.log',
+            alwaysIncludePattern: true
+        },},
+    categories: { default: { appenders: ['cheese'], level: 'debug' },
+        crash: { appenders: ['crash'], level: 'debug' }},
     pm2: true
 })
 var Logger = log4js.getLogger('cheese');
+var LoggerCrash = log4js.getLogger('crash');
 
 function debug(msg){
     Logger.debug(msg)
 }
 
+function debugCrash(msg){
+    LoggerCrash.debug(msg)
+}
+
 exports.debug = debug
+exports.debugCrash = debugCrash
--- main.js
+++ main.js
@@ -6,3 +6,16 @@ require("./lp/okex/okex_btc_kline1m")
 require("./lp/binance/binance_btc_kline1m")
 require("./lp/bitfinex/bitfinex_btc_kline1m")
 require("./httpserver/SimpleServer")
+let logger = require("./log4js/Loger")
+
+
+process.on('uncaughtException', function (err) {
+    //打印出错误
+    console.log(err);
+    //打印出错误的调用栈方便调试
+    console.log(err.stack)
+
+    logger.debugCrash(err)
+    logger.debugCrash(err.stack)
+
+});
