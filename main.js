f938b66329487cdd6c425bea23fd378790b8496e
charlieholy <357483226@qq.com>
2018-02-07 14:38:59 +0800

    add start

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-require("./ltc_kline1m")
-require("./eth_kline1m")
-require("./ticker")
-require("./lp/huopro/huopro_btc_kline1m")
-require("./lp/okex/okex_btc_kline1m")
-require("./lp/binance/binance_btc_kline1m")
-require("./lp/bitfinex/bitfinex_btc_kline1m")
-require("./httpserver/SimpleServer")
 let logger = require("./log4js/Loger")
+require("./lp/goconfig")
 
 
 process.on('uncaughtException', function (err) {
