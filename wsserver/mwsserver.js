1f109cd3a5dcb2eb71daa8100cbbe1fadc9f6170
charlie <daad777@126.com>
2018-03-06 22:34:04 +0800

    try broadCase

--- wsserver/mwsserver.js
+++ wsserver/mwsserver.js
@@ -2,18 +2,21 @@ var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server
 wss = new WebSocketServer({ port: 8081 });
 
+var g_ws = [];
+
 var num = 0;
 let clientnum = 0
 //'open', 'error', 'close', 'message'
 wss.on('connection', function(ws) {
-    console.log("conn: "+ clientnum++);
     ws.on('message', function(message) {
         console.log("msg: " + message);
         num++
         ws.send("pong" + num);
+        broadCase("num: " + num);
     });
-    ws.on('close', function () {
+    ws.on('close', function (ws) {
         console.log("onclose");
+
     });
     ws.on('error',function (err) {
         console.log("err",err);
@@ -21,4 +24,11 @@ wss.on('connection', function(ws) {
     ws.on('open',function () {
         console.log("onopen");
     })
+
 });
+
+var broadCase = function () {
+    wss.clients.forEach(function (ws) {
+        ws.send("asd");
+    })
+}
